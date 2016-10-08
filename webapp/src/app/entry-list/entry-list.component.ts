f27573b1b4d9944c8adb598b5234eacf0cc4f5cb
Alexander Ott <alexo@trayn.com>
2016-10-08 15:20:10 +0200

    Various frontend stuff

--- webapp/src/app/entry-list/entry-list.component.ts
+++ webapp/src/app/entry-list/entry-list.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {EntryService} from "../services/entry.service";
 import {Entry} from "../models/entry";
 
 @Component({
-    selector: 'hlth-log-list',
-    templateUrl: 'entry-list.component.html'
+	selector: 'hlth-entry-list',
+	templateUrl: 'entry-list.component.html'
 })
 export class EntryListComponent implements OnInit {
 
-	private loading = false;
+	private loading = true;
 	private entries: Entry[];
 
-    constructor(private entryService: EntryService) { }
+	constructor(private entryService: EntryService) {
+	}
 
-    ngOnInit() {
-
-	this.entryService.getEntries()
+	ngOnInit() {
+		this.entryService.getEntries()
 			.subscribe(
-				entries => this.entries = entries,
+				entries => {
+					this.entries = entries;
+					this.loading = false;
+				},
 				error => {
 					// TODO error handling
-				}
-			)
-
+				})
 	}
 
 }
